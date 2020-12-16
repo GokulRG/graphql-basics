0d2856c1a5b560f07565f772092772222e4f1c9e
Gokul Ramakrishnan <ramakrishnan.go@husky.neu.edu>
2020-12-16 05:16:39 -0800

    working with subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -70,7 +70,7 @@ const Mutation = {
 
 		return user;
 	},
-	createPost: (parent, args, { db }, info) => {
+	createPost: (parent, args, { db, pubsub }, info) => {
 		const isAuthorValid = db.users.some((user) => user.id === args.data.author);
 
 		if (!isAuthorValid) {
@@ -84,9 +84,13 @@ const Mutation = {
 
 		db.posts.push(post);
 
+		if (post.published) {
+			pubsub.publish('post', { post: { mutation: 'CREATED', data: post } });
+		}
+
 		return post;
 	},
-	deletePost: (parent, args, { db }, info) => {
+	deletePost: (parent, args, { db, pubsub }, info) => {
 		const postIndex = db.posts.findIndex((post) => post.id === args.id);
 
 		if (postIndex < 0) {
@@ -96,12 +100,23 @@ const Mutation = {
 		const deletedPosts = db.posts.splice(postIndex, 1);
 		db.comments = db.comments.filter((comment) => comment.post !== deletedPosts[0].id);
 
+		if (deletedPosts[0].published) {
+			pubsub.publish('post', {
+				post: {
+					mutation: 'DELETED',
+					data: deletedPosts[0]
+				}
+			});
+		}
+
 		return deletedPosts[0];
 	},
-	updatePost: (parent, args, { db }, info) => {
+	updatePost: (parent, args, { db, pubsub }, info) => {
 		const { id, data } = args;
 
 		const post = db.posts.find((post) => post.id === id);
+
+		const originalPost = { ...post };
 
 		if (!post) {
 			throw new Error('Post Not Found');
@@ -117,11 +132,37 @@ const Mutation = {
 
 		if (typeof data.published === 'boolean') {
 			post.published = data.published;
+
+			if (originalPost.published && !post.published) {
+				// deleted event
+				pubsub.publish('post', {
+					post: {
+						mutation: 'DELETED',
+						data: originalPost
+					}
+				});
+			} else if (!originalPost.published && post.published) {
+				//created event
+				pubsub.publish('post', {
+					post: {
+						mutation: 'CREATED',
+						data: post
+					}
+				});
+			}
+		} else if (post.published) {
+			//updated event
+			pubsub.publish('post', {
+				post: {
+					mutation: 'UPDATED',
+					data: post
+				}
+			});
 		}
 
 		return post;
 	},
-	createComment: (parent, args, { db }, info) => {
+	createComment: (parent, args, { db, pubsub }, info) => {
 		const isAuthorValid = db.users.some((user) => user.id === args.data.author);
 		const isPostValidAndPublished = db.posts.some((post) => {
 			return post.id === args.data.post && post.published;
@@ -142,9 +183,12 @@ const Mutation = {
 
 		db.comments.push(comment);
 
+		// Subscription
+		pubsub.publish(`comment ${args.data.post}`, { comment: { mutation: 'CREATED', data: comment } });
+
 		return comment;
 	},
-	deleteComment: (parent, args, { db }, info) => {
+	deleteComment: (parent, args, { db, pubsub }, info) => {
 		const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
 
 		if (commentIndex < 0) {
@@ -153,23 +197,26 @@ const Mutation = {
 
 		const removedComments = db.comments.splice(commentIndex, 1);
 
+		pubsub.publish(`comment ${removedComments[0].post}`, { comment: { mutation: 'DELETED', data: removedComments[0] } });
 		return removedComments[0];
-    },
-    updateComment: (parent, args, { db }, info) => {
-        const { id, data } = args;
+	},
+	updateComment: (parent, args, { db, pubsub }, info) => {
+		const { id, data } = args;
 
-        const comment = db.comments.find(comment => comment.id === id);
+		const comment = db.comments.find((comment) => comment.id === id);
 
-        if (!comment) {
-            throw new Error('Comment not Found!');
+		if (!comment) {
+			throw new Error('Comment not Found!');
+		}
+
+		if (typeof data.text === 'string') {
+			comment.text = data.text;
         }
+        
+        pubsub.publish(`comment ${comment.post}`, { comment: { mutation: 'UPDATED', data: comment } });
 
-        if (typeof data.text === 'string') {
-            comment.text = data.text;
-        }
-
-        return comment;
-    }
+		return comment;
+	}
 };
 
 export default Mutation;
